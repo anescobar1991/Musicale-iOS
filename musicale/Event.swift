ecaba54e82a5b8880a4ca7f85cdb22e67676f860
Andres Escobar <anescobar1991@gmail.com>
2015-04-18 21:34:58 -0700

    First commit of Musicale-iOS

---
+++ musicale/Event.swift
@@ -0,0 +1,23 @@
+//
+//  Event.swift
+//  musicale
+//
+//  Created by Andres Escobar on 4/14/15.
+//  Copyright (c) 2015 Andres Escobar. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+    var title: String
+    var date: String
+    var location: String
+    var imageUrl: String
+    
+    init(title: String, date: String, location: String, imageUrl: String) {
+        self.title = title
+        self.date = date
+        self.location = location
+        self.imageUrl = imageUrl
+    }
+}
---
+++ musicale/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  musicale
+//
+//  Created by Andres Escobar on 4/14/15.
+//  Copyright (c) 2015 Andres Escobar. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var whenWhereLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
