04fe252339429f13067e1089e14ebe2eb0ce3d09
Andres Escobar <anescobar1991@gmail.com>
2015-06-28 22:08:33 -0700

    Added get current location button on change location screen

--- musicale/LocationChangeTableViewController.swift
+++ musicale/LocationChangeTableViewController.swift
@@ -14,14 +14,59 @@ class LocationChangeTableViewController: UIViewController {
   @IBOutlet weak var searchBar: UISearchBar!
   @IBOutlet weak var locationsTableView: UITableView!
   
+  private var locationManager = CLLocationManager()
+  private var dataManager = PersistentDataManager.sharedInstance
+
   private var places: [CLPlacemark] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+    
     searchBar.delegate = self
   }
+  
+  private func determineLocationServicesAuthorization(status: CLAuthorizationStatus) {
+    
+      switch status {
+      case .AuthorizedWhenInUse, .AuthorizedAlways:
+          locationManager.startUpdatingLocation()
+      case .NotDetermined:
+        locationManager.requestWhenInUseAuthorization()
+      case .Restricted, .Denied:
+        let alertController = UIAlertController(
+          title: "Location Access Disabled",
+          message: "In order to get events near you we need to know where you are silly! Please open this app's settings and set location access to 'While Using the App'.",
+          preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let openAction = UIAlertAction(title: "Open Settings", style: .Default) { (action) in
+          if let url = NSURL(string:UIApplicationOpenSettingsURLString) {
+            UIApplication.sharedApplication().openURL(url)
+          }
+        }
+        alertController.addAction(openAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+      }
+    }
 
+  
+  @IBAction func onUseCurrentLocationTouchUp(sender: AnyObject) {
+    determineLocationServicesAuthorization(CLLocationManager.authorizationStatus())
+    let button = sender as! UIButton
+    button.backgroundColor = UIColor.whiteColor()
+  }
+  
+  @IBAction func onUseCurrentLocationTouchDown(sender: AnyObject) {
+    let button = sender as! UIButton
+    button.backgroundColor = UIColor.lightGrayColor()
+  }
+  
 }
 
 // MARK: - UISearchBarDelegate
@@ -71,22 +116,16 @@ extension LocationChangeTableViewController : UITableViewDataSource {
     
     let cell = tableView.dequeueReusableCellWithIdentifier("locationResultCell", forIndexPath: indexPath) as! UITableViewCell
     
-    cell.textLabel?.text = sanitizePlaceToDisplay(places[indexPath.row])
+    let place = places[indexPath.row]
+    
+    cell.textLabel?.text = place.name
+    if let locality = place.locality {
+      cell.detailTextLabel?.text = "\(place.locality), \(place.administrativeArea) \(place.country)"
+    } else {
+      cell.detailTextLabel?.text = "\(place.administrativeArea) \(place.country)"
+    }
 
     return cell
-  }
-  
-  private func sanitizePlaceToDisplay(place: CLPlacemark) -> String {
-    var placeStringArray :[String] = []
-    
-    if let name = place.name { placeStringArray.append("\(name) -") }
-    if let locality = place.locality { placeStringArray.append("\(locality),") }
-    
-    placeStringArray.append(place.country)
-    
-    let sanitizedPlaceString = " ".join(placeStringArray)
-
-    return sanitizedPlaceString
   }
   
 }
@@ -94,10 +133,29 @@ extension LocationChangeTableViewController : UITableViewDataSource {
 extension LocationChangeTableViewController: UITableViewDelegate {
   
   func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    let dataManager = PersistentDataManager.sharedInstance
-    
     dataManager.searchLocation = places[indexPath.row].location
     dataManager.searchPlace = places[indexPath.row]
+  }
+  
+}
+
+// MARK: - CLLocationManagerDelegate
+extension LocationChangeTableViewController: CLLocationManagerDelegate {
+//  func locationManager(manager: CLLocationManager!,
+//    didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+//      
+//      determineLocationServicesAuthorization(status)
+//  }
+  
+  func locationManager(manager: CLLocationManager!,
+    didUpdateLocations locations: [AnyObject]!) {
+      locationManager.stopUpdatingLocation()
+      
+      let latestLocation = locations[locations.count - 1] as! CLLocation
+      
+      dataManager.searchLocation = latestLocation
+      dataManager.searchPlace = nil
+      performSegueWithIdentifier("unwindToMoreView", sender: nil)
   }
   
 }
